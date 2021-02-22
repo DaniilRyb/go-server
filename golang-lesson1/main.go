bfa0e644cd7e020a21613312a0e158b5c23b4e74
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2021-02-22 11:50:04 +0300

    go

---
+++ golang-lesson1/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+//  fmt.Println("Привет, Денис!!!", "Привет, Данил!!!", "Привет, Даша!!!")
+  //fmt.Println("Привет, Денис!!!")
+  //fmt.Println("Привет, Денис!!!")
+  var var1 int = 1e6
+  var var2 int = 1000000
+  var varSum = var1 + var2
+  fmt.Println("сумма двух чисел =", varSum)
+}
